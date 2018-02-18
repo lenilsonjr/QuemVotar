452ae5342cb362b17851e39843a05ba0507fd4e4
Luan Vicente <hello@idlua.me>
2018-02-18 01:58:00 -0300

    GET /questions and storage the customer answers

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,5 +1,57 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const App = () => (<h1>Quem Votar</h1>)
+class App extends Component  {
+  state = { answers: [], questions: [] }
+
+  componentWillMount () {
+    const endpoint = 'http://localhost:3001'
+
+    fetch(`${endpoint}/questions`)
+      .then(res => res.json())
+      .then(res => this.setState({questions: res}))
+      .then(() => this.setState({questionsTotal: this.state.questions.length}))
+      .then(() => this.setState({currentQuestion: this.state.questions[0]}))
+      .catch(err => {throw err})
+  }
+
+  handleSubmit (e) {
+    e.preventDefault()
+
+    const {id} = this.state.currentQuestion
+    const answer = e.target.value === '0' ? true : false
+
+    this.setState({answers: [...this.state.answers, {id, answer}]})
+    this.setState({currentQuestion: this.state.questions[id + 1]})
+  }
+
+  render () {
+    const {currentQuestion} = this.state
+
+    return (
+      <section>
+        <h1>Quem Votar — Problemas para decidir seu voto?</h1>
+
+        <form>
+          {currentQuestion &&
+            <div key={currentQuestion.id}>
+              <h2>{currentQuestion.title}</h2>
+
+              {currentQuestion.choices.map((choice, index) => (
+                <button
+                  onClick={this.handleSubmit.bind(this)}
+                  ref='answer'
+                  key={index}
+                  value={index}
+                >
+                  {choice}
+                </button>
+              ))}
+            </div>
+          }
+        </form>
+      </section>
+    )
+  }
+}
 
 export default App
