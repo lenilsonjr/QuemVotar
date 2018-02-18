e664a4085b11fc86d8a893e34fac3e86ae7ff563
Luan Vicente <hello@idlua.me>
2018-02-18 11:06:07 -0300

    Resolve POST /answers response

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react'
 
+const SETTINGS = { API_HOST: 'http://localhost:3001' }
+
 class App extends Component  {
-  state = { answers: [], questions: [] }
+  state = {
+    answers: [],
+    questions: [],
+    hasQuestions: false,
+    showSendAnswersButton: true
+  }
 
   componentWillMount () {
-    const endpoint = 'http://localhost:3001'
-
-    fetch(`${endpoint}/questions`)
+    fetch(`${SETTINGS.API_HOST}/questions`)
       .then(res => res.json())
       .then(res => this.setState({questions: res}))
       .then(() => this.setState({questionsTotal: this.state.questions.length}))
+      .then(res => this.setState({hasQuestions: this.state.questionsTotal}))
       .then(() => this.setState({currentQuestion: this.state.questions[0]}))
       .catch(err => {throw err})
   }
@@ -22,17 +28,42 @@ class App extends Component  {
 
     this.setState({answers: [...this.state.answers, {id, answer}]})
     this.setState({currentQuestion: this.state.questions[id + 1]})
+    this.setState({hasQuestions: this.state.hasQuestions - 1})
+  }
+
+  sendAnswers () {
+    // fetch(`${SETTINGS.API_HOST}/answers`,
+    //   {
+    //     method: 'POST',
+    //     headers: { Content-Type: 'application/json', Accept: 'application/json' },
+    //     body: JSON.stringify(body)
+    //   }
+    // )
+    // .then()
+
+    this.setState({candidates: [{
+      name: 'candidate',
+      photo: 'http://fakeimg.pl/300',
+      description: 'description'
+    }]})
+
+    this.setState({showSendAnswersButton: false})
   }
 
   render () {
-    const {currentQuestion} = this.state
+    const {
+      currentQuestion,
+      hasQuestions,
+      candidates,
+      showSendAnswersButton
+    } = this.state
 
     return (
       <section>
         <h1>Quem Votar — Problemas para decidir seu voto?</h1>
 
-        <form>
-          {currentQuestion &&
+        {currentQuestion &&
+          <form>
             <div key={currentQuestion.id}>
               <h2>{currentQuestion.title}</h2>
 
@@ -47,8 +78,32 @@ class App extends Component  {
                 </button>
               ))}
             </div>
-          }
-        </form>
+          </form>
+        }
+
+        {!hasQuestions &&
+          <section>
+            {showSendAnswersButton &&
+              <button onClick={this.sendAnswers.bind(this)}>
+                Em quem devo votar?
+              </button>
+            }
+
+            {candidates && candidates.map(candidate => (
+              <section key={candidate.name}>
+                <figure>
+                  <img
+                    src={candidate.photo}
+                    alt='Foto de um candidato à política no Brasil'
+                  />
+                  <figcaption>{candidate.description}</figcaption>
+                </figure>
+
+                <h2>{candidate.name}</h2>
+              </section>
+            ))}
+          </section>
+        }
       </section>
     )
   }
